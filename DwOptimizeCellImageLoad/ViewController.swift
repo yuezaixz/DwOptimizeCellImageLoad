736b6b2e1cc1f4bea1a3496bfba842485d58d80f
吴迪玮 <wudiwei@vv.cn>
2020-05-27 18:27:14 +0800

    逻辑调整

--- DwOptimizeCellImageLoad/ViewController.swift
+++ DwOptimizeCellImageLoad/ViewController.swift
@@ -15,8 +15,6 @@ class ViewController: UIViewController {
     
     var finalPureLand: CGRect?
     
-    var isDeceleratingOrStop = true
-    
     let imageUrlStrs = ["http://qiniu.xingheaoyou.com/1.jpg","http://qiniu.xingheaoyou.com/2.jpg","http://qiniu.xingheaoyou.com/3.jpg","http://qiniu.xingheaoyou.com/4.jpg","http://qiniu.xingheaoyou.com/5.jpg","http://qiniu.xingheaoyou.com/6.jpeg","http://qiniu.xingheaoyou.com/7.jpeg","http://qiniu.xingheaoyou.com/8.jpeg"]
 
     override func viewDidLoad() {
@@ -48,10 +46,8 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ImageTableViewCell.kReuseIdentifier, for: indexPath) as! ImageTableViewCell
-        if isDeceleratingOrStop && finalPureLand == nil {
-            let urlStr = indexPath.row / 10 % 5 == 1 ? imageUrlStrs[indexPath.row % 8] : (imageUrlStrs[indexPath.row % 8] + "?random=\(indexPath.row)")
-            cell.loadImage(imageUrlStr: urlStr)
-        }
+//        let urlStr = indexPath.row / 10 % 5 == 1 ? imageUrlStrs[indexPath.row % 8] : (imageUrlStrs[indexPath.row % 8] + "?random=\(indexPath.row)")
+//        cell.loadImage(imageUrlStr: urlStr)
         return cell
     }
 }
@@ -60,7 +56,6 @@ extension ViewController: UIScrollViewDelegate {
     
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
         finalPureLand = nil
-        isDeceleratingOrStop = false
         
         loadImageIfNeed()
     }
@@ -69,12 +64,12 @@ extension ViewController: UIScrollViewDelegate {
         finalPureLand = CGRect(x: targetContentOffset.pointee.x, y: targetContentOffset.pointee.y, width: scrollView.frame.size.width, height: scrollView.frame.size.height)
     }
     
-    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
-        isDeceleratingOrStop = true
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        finalPureLand = nil
+        loadImageIfNeed()
     }
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        isDeceleratingOrStop = true
         finalPureLand = nil
         loadImageIfNeed()
     }
@@ -98,20 +93,25 @@ class ImageTableViewCell: UITableViewCell {
     
     @IBOutlet weak var avatarImageView: UIImageView!
     let cache = KingfisherManager.shared.cache
+    var isLoad = false
     
     override func prepareForReuse() {
         avatarImageView.image = nil
     }
     
     func loadImage(imageUrlStr: String) {
+        isLoad = true
         avatarImageView.kf.setImage(with: URL(string: imageUrlStr))
     }
 
     func loadImage(imageUrlStr: String, landRect: CGRect?, cellFrame: CGRect) {
-        if let landRect = landRect, !landRect.intersects(cellFrame), let image = ImageCache.default.retrieveImageInMemoryCache(forKey: imageUrlStr) {
+        isLoad = true
+        if let landRect = landRect, !landRect.intersects(cellFrame) {
             // 如果滑动中为减速，则只加载内存中的。
             // 如果加载硬盘中的，硬盘中的加载后要解析转换成位图，也会造成卡顿
-            avatarImageView.image = image
+            if let image = ImageCache.default.retrieveImageInMemoryCache(forKey: imageUrlStr) {
+                avatarImageView.image = image
+            }
         } else {
             loadImage(imageUrlStr: imageUrlStr)
         }
